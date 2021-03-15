81a6cae46c20d97f025d2d9878ed6580c4ab3e79
Anish Narayanan <anish.narayanan32@gmail.com>
2021-03-14 23:52:45 -0500

    Finally can display a chess.com game position (not the pocket though, and only one board...)

--- app.py
+++ app.py
@@ -1,28 +1,83 @@
+import operator as op
 from flask import Flask, render_template, escape, request, redirect
+from bughouse import BughouseBoard
+import chess
 import chess_com
 import tcn_parser
 
 app = Flask(__name__)
 
+#game 1: 9494798777
+#game 2: 9494798775
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
+def process_game_id(board_num, game_id):
+    game_data = chess_com.fetch_game(game_id)
+    raw_game_moves = game_data["game"]["moveList"]
+    game_moves = tcn_parser.decode_tcn(raw_game_moves)
+    raw_game_move_times = game_data["game"]["moveTimestamps"]
+    game_move_times = [int(move_time) for move_time in raw_game_move_times.split(",")]
+
+    prev_lost_time = 0
+    total_time = 0
+    processed_moves = []
+    for i, move_data in enumerate(zip(game_moves, game_move_times)):
+        move, move_time = move_data
+        curr_lost_time = 1800 - move_time
+        total_time = prev_lost_time + curr_lost_time
+        prev_lost_time = curr_lost_time
+        moving_player = i % 2 == 0
+        processed_move = (board_num, moving_player, move, move_time, total_time)
+        processed_moves.append(processed_move)
+    return processed_moves
+
+def merge_processed_moves(first_moves, second_moves):
+    bug_board = BughouseBoard()
+    all_raw_moves = first_moves + second_moves
+    all_raw_moves.sort(key=op.itemgetter(-1))
+    all_moves = []
+    for raw_move in all_raw_moves:
+        board_num, moving_player, move, move_time, total_time = raw_move
+        #print(board_num, moving_player, move)
+        move_obj = chess.Move(
+            from_square=chess.parse_square(move["from"]) if move["from"] != None else chess.A1,
+            to_square=chess.parse_square(move["to"]),
+            promotion=chess.Piece.from_symbol(move["promotion"]).piece_type if move["promotion"] != None else None,
+            drop=chess.Piece.from_symbol(move["drop"]).piece_type if move["drop"] != None else None,
+        )
+        #print(move)
+        bug_board.move(board_num, move_obj)
+        fens = bug_board.get_fens()
+        first_board_fen, second_board_fen = fens
+        #print(fens)
+
+        processed_move = {
+            "board_num": board_num,
+            "moving_player": moving_player,
+            "from": move["from"],
+            "to": move["to"],
+            "drop": move["drop"],
+            "promotion": move["promotion"],
+            "move_time": move_time,
+            "total_time": total_time,
+            "first_board_fen": first_board_fen,
+            "second_board_fen": second_board_fen,
+        }
+        all_moves.append(processed_move)
+    return all_moves
+
 @app.route("/game", methods=["POST"])
 def load_game():
     first_game_id = request.form["first_game_id"]
-    first_game_data = chess_com.fetch_game(first_game_id)
-    first_game_moves = tcn_parser.decode_tcn(first_game_data["game"]["moveList"])
     second_game_id = request.form["second_game_id"]
-    second_game_data = chess_com.fetch_game(second_game_id)
-    second_game_moves = tcn_parser.decode_tcn(second_game_data["game"]["moveList"])
+    first_moves = process_game_id(0, first_game_id)
+    second_moves = process_game_id(1, second_game_id)
+    moves = merge_processed_moves(first_moves, second_moves)
     data = {
-        "first_game_id": first_game_id,
-        "first_game_data": first_game_data,
-        "first_game_moves": first_game_moves,
-        "second_game_id": second_game_id,
-        "second_game_data": second_game_data,
-        "second_game_moves": second_game_moves,
+        "moves": moves,
     }
     return render_template("game.html", data=data)
 
--- static/js/game.js
+++ static/js/game.js
@@ -1,6 +1,134 @@
-console.log(first_game_id);
-console.log(first_game_data);
-console.log(first_game_moves);
-console.log(second_game_id);
-console.log(second_game_data);
-console.log(second_game_moves);
+var app = new PIXI.Application({
+    width: 400, 
+    height: 400, 
+    backgroundColor: 0x1099bb, 
+    resolution: window.devicePixelRatio || 1,
+    antialias: true,
+});
+
+document.getElementById("first_game_board").appendChild(app.view);
+
+var file_paths = {
+    "p": "static/img/chesspieces/alpha/bP.png",
+    "n": "static/img/chesspieces/alpha/bN.png",
+    "b": "static/img/chesspieces/alpha/bB.png",
+    "r": "static/img/chesspieces/alpha/bR.png",
+    "q": "static/img/chesspieces/alpha/bQ.png",
+    "k": "static/img/chesspieces/alpha/bK.png",
+    "P": "static/img/chesspieces/alpha/wP.png",
+    "N": "static/img/chesspieces/alpha/wN.png",
+    "B": "static/img/chesspieces/alpha/wB.png",
+    "R": "static/img/chesspieces/alpha/wR.png",
+    "Q": "static/img/chesspieces/alpha/wQ.png",
+    "K": "static/img/chesspieces/alpha/wK.png",
+}
+
+var textures = {};
+var key;
+var file_path;
+for(key in file_paths)
+{
+    file_path = file_paths[key];
+    textures[key] = new PIXI.Texture.from(file_path);
+}
+var square_size = 50;
+var light_color = 0xEEEED2;
+var dark_color = 0x769656;
+var num_files = 8;
+var num_ranks = 8;
+
+function draw_board(app, square_size, light_color, dark_color)
+{
+    var grid = new PIXI.Graphics();
+    var i = 0;
+    var j = 0;
+    for(i = 0; i < 8; i++)
+    {
+        for(j = 0; j < 8; j++)
+        {
+            if(i % 2 == j % 2)
+            {
+                grid.beginFill(light_color);
+            }
+            else
+            {
+                grid.beginFill(dark_color);
+            }    
+            grid.drawRect(i * square_size, j * square_size, square_size, square_size);
+        }
+    }
+    app.stage.addChild(grid);
+}
+
+function draw_piece(app, square_size, piece_type, file, rank)
+{
+    var sprite = new PIXI.Sprite(textures[piece_type]);
+    app.stage.addChild(sprite);
+    sprite.x = file * square_size;
+    sprite.y = (num_ranks - rank - 1) * square_size;
+    sprite.width = square_size;
+    sprite.height = square_size;
+}
+
+function parse_fen(fen)
+{
+    var parts = fen.split(" ");
+    var position = parts[0];
+    var active_color = parts[1];
+    var castling = parts[2];
+    var en_passant = parts[3];
+    var half_move_clock = parts[4];
+    var full_move_number = parts[5];
+
+    var pocket = position.split("[")[1].split("]")[0];
+    var ranks = position.split("[")[0].split("/")
+    var piece_positions = []
+
+    var rank;
+    var rank_char_index;
+    var rank_char;
+    var rank_pos;
+    var file_pos;
+    var rank_char_shift;
+    var square = {};
+    var piece_pos;
+    for(rank_pos = 0; rank_pos < ranks.length; rank_pos++)
+    {
+        file_pos = 0;
+        rank = ranks[rank_pos].split("");
+        for(rank_char_index = 0; rank_char_index < rank.length; rank_char_index++)
+        {
+            rank_char = rank[rank_char_index];
+            rank_char_shift = parseInt(rank_char)
+            if(!isNaN(rank_char_shift))
+            {
+                file_pos += rank_char_shift;
+            }
+            else
+            {
+                piece_pos = [file_pos, ranks.length - rank_pos - 1, rank_char];
+                piece_positions.push(piece_pos);
+                file_pos += 1;
+            }
+        }
+    }
+
+    return piece_positions;
+}
+
+function draw_position(app, fen)
+{
+    var piece_pos;
+    var i;
+    piece_positions = parse_fen(fen);
+    console.log(piece_positions);
+    draw_board(app, square_size, light_color, dark_color);
+    for(i = 0; i < piece_positions.length; i++)
+    {
+        piece_pos = piece_positions[i];
+        console.log(piece_pos);
+        draw_piece(app, square_size, piece_pos[2], piece_pos[0], piece_pos[1]);
+    }
+}
+
+draw_position(app, moves[moves.length - 1].first_board_fen);
