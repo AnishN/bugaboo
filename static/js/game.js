558336c11da7d857ee4295b32f3b1e17dfe726f5
Anish Narayanan <anish.narayanan32@gmail.com>
2021-03-15 17:29:54 -0500

    Implemented basic pocket support, with displayed counts!

--- static/js/game.js
+++ static/js/game.js
@@ -1,12 +1,362 @@
-var app = new PIXI.Application({
-    width: 400, 
-    height: 400, 
-    backgroundColor: 0x1099bb, 
+const text_style = new PIXI.TextStyle({
+    fontFamily: 'Arial',
+    fontSize: 20,
+    fill: "#ffffff",
+    stroke: '#769656',
+    strokeThickness: 2,
+    dropShadow: true,
+    dropShadowColor: '#000000',
+    dropShadowBlur: 2,
+    dropShadowAngle: Math.PI / 4,
+    dropShadowDistance: 2,
+    wordWrap: true,
+    wordWrapWidth: 440,
+    lineJoin: 'round',
+    align: "center",
+});
+
+class ChessBoardPocket
+{
+    constructor(board_size, pocket_size, pocket_color, piece_textures, piece_color)
+    {
+        this.board_size = board_size;
+        this.pocket_size = pocket_size;
+        this.pocket_color = pocket_color;
+        this.piece_textures = piece_textures;
+
+        this.pawn = new PIXI.Sprite();
+        this.knight = new PIXI.Sprite();
+        this.bishop = new PIXI.Sprite();
+        this.rook = new PIXI.Sprite();
+        this.queen = new PIXI.Sprite();
+        this.pieces = [this.pawn, this.knight, this.bishop, this.rook, this.queen];
+        this.set_color(piece_color);
+        this.graphics = new PIXI.Graphics();
+    }
+
+    set_color(piece_color)
+    {
+        this.piece_color = piece_color
+        if(this.piece_color == true)//white
+        {
+            this.pawn.texture = this.piece_textures["P"];
+            this.knight.texture = this.piece_textures["N"];
+            this.bishop.texture = this.piece_textures["B"];
+            this.rook.texture = this.piece_textures["R"];
+            this.queen.texture = this.piece_textures["Q"];
+        }
+        else//black
+        {
+            this.pawn.texture = this.piece_textures["p"];
+            this.knight.texture = this.piece_textures["n"];
+            this.bishop.texture = this.piece_textures["b"];
+            this.rook.texture = this.piece_textures["r"];
+            this.queen.texture = this.piece_textures["q"];
+        }
+    }
+
+    draw_pocket(piece_pockets)
+    {
+        var i;
+        var piece;
+        var piece_char;
+        var piece_chars;
+        var piece_count;
+        var piece_count_text;
+        var white_piece_chars = ["P", "N", "B", "R", "Q"];
+        var black_piece_chars = ["p", "n", "b", "r", "q"];
+        
+        this.graphics.removeChildren();
+        this.graphics.beginFill(this.pocket_color);
+        this.graphics.drawRect(0, 0, this.board_size, this.pocket_size);
+        this.graphics.endFill(this.pocket_color);
+        
+        if(this.piece_color == true)//white
+        {
+            piece_chars = white_piece_chars;
+        }
+        else//black
+        {
+            piece_chars = black_piece_chars;
+        }
+        
+        for(i = 0; i < piece_chars.length; i++)
+        {
+            piece_char = piece_chars[i];
+            piece = this.pieces[i];
+            piece_count = piece_pockets[piece_char];
+            if(piece_count == 0)
+            {
+                piece.alpha = 0.1;
+            }
+            else
+            {
+                piece.alpha = 1.0;
+            }
+            piece.x = i * this.pocket_size;
+            piece.y = 0;
+            piece.width = this.pocket_size;
+            piece.height = this.pocket_size;
+            this.graphics.addChild(piece);
+
+            if(piece_count != 0)
+            {
+                piece_count_text = new PIXI.Text(piece_count, text_style);
+                piece_count_text.pivot.set(
+                    piece_count_text.width / 2, 
+                    piece_count_text.height / 2
+                );
+                piece_count_text.x = this.pocket_size / 2 + (i * this.pocket_size);
+                piece_count_text.y = this.pocket_size / 2;
+                this.graphics.addChild(piece_count_text);
+            }
+        }
+    }
+}
+
+class ChessBoard
+{
+    constructor(board_size, light_color, dark_color, piece_textures, flip, pocket_size, pocket_color)
+    {
+        this.board_size = board_size;
+        this.light_color = light_color;
+        this.dark_color = dark_color;
+        this.flip = flip;
+        this.pocket_size = pocket_size;
+        this.pocket_color = pocket_color;
+        this.graphics = new PIXI.Graphics();
+        this.piece_textures = piece_textures;
+        this.top_pocket = new ChessBoardPocket(board_size, pocket_size, pocket_color, piece_textures, !flip);
+        this.bottom_pocket = new ChessBoardPocket(board_size, pocket_size, pocket_color, piece_textures, flip);
+    }
+
+    parse_fen_piece_positions(fen)
+    {
+        var parts = fen.split(" ");
+        var position = parts[0];
+        /*
+        var active_color = parts[1];
+        var castling = parts[2];
+        var en_passant = parts[3];
+        var half_move_clock = parts[4];
+        var full_move_number = parts[5];
+        */
+
+        var ranks = position.split("[")[0].split("/")
+        var piece_positions = []
+        var rank;
+        var rank_char_index;
+        var rank_char;
+        var rank_pos;
+        var file_pos;
+        var rank_char_shift;
+        var square = {};
+        var piece_pos;
+        for(rank_pos = 0; rank_pos < ranks.length; rank_pos++)
+        {
+            file_pos = 0;
+            rank = ranks[rank_pos].split("");
+            for(rank_char_index = 0; rank_char_index < rank.length; rank_char_index++)
+            {
+                rank_char = rank[rank_char_index];
+                rank_char_shift = parseInt(rank_char)
+                if(!isNaN(rank_char_shift))
+                {
+                    file_pos += rank_char_shift;
+                }
+                else
+                {
+                    piece_pos = [file_pos, ranks.length - rank_pos - 1, rank_char];
+                    piece_positions.push(piece_pos);
+                    file_pos += 1;
+                }
+            }
+        }
+        return piece_positions;
+    }
+
+    parse_fen_piece_pockets(fen)
+    {
+        var parts = fen.split(" ");
+        var position = parts[0];
+        var pocket = position.split("[")[1].split("]")[0];
+        var piece_pockets = {
+            "P": 0,
+            "N": 0,
+            "B": 0,
+            "R": 0,
+            "Q": 0,
+            "p": 0,
+            "n": 0,
+            "b": 0,
+            "r": 0,
+            "q": 0,
+        }
+        var piece_char;
+        var pocket_pos;
+        for(pocket_pos = 0; pocket_pos < pocket.length; pocket_pos++)
+        {
+            piece_char = pocket[pocket_pos];
+            piece_pockets[piece_char] += 1;
+        }
+        return piece_pockets;
+    }
+
+    draw_squares()
+    {
+        var i = 0;
+        var j = 0;
+        var square_size = this.board_size / 8;
+
+        if(this.flip == false)
+        {
+            for(i = 0; i < 8; i++)
+            {
+                for(j = 0; j < 8; j++)
+                {
+                    if(i % 2 == j % 2)
+                    {
+                        this.graphics.beginFill(this.light_color);
+                    }
+                    else
+                    {
+                        this.graphics.beginFill(this.dark_color);
+                    }    
+                    this.graphics.drawRect(
+                        i * square_size, 
+                        j * square_size + this.pocket_size, 
+                        square_size, 
+                        square_size
+                    );
+                }
+            }
+        }
+        else
+        {
+            for(i = 0; i < 8; i++)
+            {
+                for(j = 0; j < 8; j++)
+                {
+                    if(i % 2 != j % 2)
+                    {
+                        this.graphics.beginFill(this.light_color);
+                    }
+                    else
+                    {
+                        this.graphics.beginFill(this.dark_color);
+                    }    
+                    this.graphics.drawRect(
+                        i * square_size, 
+                        j * square_size + this.pocket_size, 
+                        square_size, 
+                        square_size
+                    );
+                }
+            }
+        }
+    }
+    
+    draw_piece(piece_type, file, rank)
+    {
+        var sprite = new PIXI.Sprite(this.piece_textures[piece_type]);
+        var square_size = this.board_size / 8;
+        this.graphics.addChild(sprite);
+        sprite.x = file * square_size;
+        if(this.flip == false)
+        {
+            sprite.y = (num_ranks - rank - 1) * square_size;
+        }
+        else
+        {
+            sprite.y = rank * square_size;
+        }
+        sprite.y += this.pocket_size;
+        sprite.width = square_size;
+        sprite.height = square_size;
+    }
+
+    draw_pockets(piece_pockets)
+    {
+        this.graphics.addChild(this.top_pocket.graphics);
+        this.top_pocket.graphics.x = 0;
+        this.top_pocket.graphics.y = 0;
+        this.top_pocket.draw_pocket(piece_pockets);
+
+        this.graphics.addChild(this.bottom_pocket.graphics);
+        this.bottom_pocket.graphics.x = 0;
+        this.bottom_pocket.graphics.y = this.pocket_size + this.board_size;
+        this.bottom_pocket.draw_pocket(piece_pockets);
+    }
+
+    draw_position(fen)
+    {
+        var piece_positions = this.parse_fen_piece_positions(fen);
+        var piece_pockets = this.parse_fen_piece_pockets(fen);
+        var piece_pos;
+        var i;
+
+        this.graphics.removeChildren();
+        this.draw_squares();
+        for(i = 0; i < piece_positions.length; i++)
+        {
+            piece_pos = piece_positions[i];
+            this.draw_piece(piece_pos[2], piece_pos[0], piece_pos[1]);
+        }
+        this.draw_pockets(piece_pockets);
+    }
+}
+
+function redraw_boards()
+{
+    first_board.draw_position(moves[curr_move_index].first_board_fen);
+    second_board.draw_position(moves[curr_move_index].second_board_fen);
+}
+
+document.getElementById("head_button").addEventListener("click", function() {
+    curr_move_index = 0;
+    redraw_boards();
+});
+
+document.getElementById("prev_button").addEventListener("click", function() {
+    curr_move_index -= 1;
+    if(curr_move_index < 0)
+    {
+        curr_move_index = 0;
+    }
+    redraw_boards();
+});
+
+document.getElementById("next_button").addEventListener("click", function() {
+    curr_move_index += 1;
+    if(curr_move_index == moves.length)
+    {
+        curr_move_index = moves.length - 1;
+    }
+    redraw_boards();
+}); 
+
+document.getElementById("tail_button").addEventListener("click", function() {
+    curr_move_index = moves.length - 1;
+    redraw_boards();
+});
+
+var light_color = 0xEEEED2;
+var dark_color = 0x769656;
+var num_files = 8;
+var num_ranks = 8;
+var padding = 10;
+var pocket_size = 380/8;
+var pocket_color = 0x777777;
+
+const app = new PIXI.Application({
+    width: 800, 
+    height: 500, 
+    backgroundColor: 0x4D4D4D, 
     resolution: window.devicePixelRatio || 1,
     antialias: true,
 });
 
-document.getElementById("first_game_board").appendChild(app.view);
+document.getElementById("game").appendChild(app.view);
 
 var file_paths = {
     "p": "static/img/chesspieces/alpha/bP.png",
@@ -23,112 +373,23 @@ var file_paths = {
     "K": "static/img/chesspieces/alpha/wK.png",
 }
 
-var textures = {};
+var piece_textures = {};
 var key;
 var file_path;
 for(key in file_paths)
 {
     file_path = file_paths[key];
-    textures[key] = new PIXI.Texture.from(file_path);
-}
-var square_size = 50;
-var light_color = 0xEEEED2;
-var dark_color = 0x769656;
-var num_files = 8;
-var num_ranks = 8;
-
-function draw_board(app, square_size, light_color, dark_color)
-{
-    var grid = new PIXI.Graphics();
-    var i = 0;
-    var j = 0;
-    for(i = 0; i < 8; i++)
-    {
-        for(j = 0; j < 8; j++)
-        {
-            if(i % 2 == j % 2)
-            {
-                grid.beginFill(light_color);
-            }
-            else
-            {
-                grid.beginFill(dark_color);
-            }    
-            grid.drawRect(i * square_size, j * square_size, square_size, square_size);
-        }
-    }
-    app.stage.addChild(grid);
+    piece_textures[key] = new PIXI.Texture.from(file_path);
 }
 
-function draw_piece(app, square_size, piece_type, file, rank)
-{
-    var sprite = new PIXI.Sprite(textures[piece_type]);
-    app.stage.addChild(sprite);
-    sprite.x = file * square_size;
-    sprite.y = (num_ranks - rank - 1) * square_size;
-    sprite.width = square_size;
-    sprite.height = square_size;
-}
-
-function parse_fen(fen)
-{
-    var parts = fen.split(" ");
-    var position = parts[0];
-    var active_color = parts[1];
-    var castling = parts[2];
-    var en_passant = parts[3];
-    var half_move_clock = parts[4];
-    var full_move_number = parts[5];
-
-    var pocket = position.split("[")[1].split("]")[0];
-    var ranks = position.split("[")[0].split("/")
-    var piece_positions = []
-
-    var rank;
-    var rank_char_index;
-    var rank_char;
-    var rank_pos;
-    var file_pos;
-    var rank_char_shift;
-    var square = {};
-    var piece_pos;
-    for(rank_pos = 0; rank_pos < ranks.length; rank_pos++)
-    {
-        file_pos = 0;
-        rank = ranks[rank_pos].split("");
-        for(rank_char_index = 0; rank_char_index < rank.length; rank_char_index++)
-        {
-            rank_char = rank[rank_char_index];
-            rank_char_shift = parseInt(rank_char)
-            if(!isNaN(rank_char_shift))
-            {
-                file_pos += rank_char_shift;
-            }
-            else
-            {
-                piece_pos = [file_pos, ranks.length - rank_pos - 1, rank_char];
-                piece_positions.push(piece_pos);
-                file_pos += 1;
-            }
-        }
-    }
-
-    return piece_positions;
-}
-
-function draw_position(app, fen)
-{
-    var piece_pos;
-    var i;
-    piece_positions = parse_fen(fen);
-    console.log(piece_positions);
-    draw_board(app, square_size, light_color, dark_color);
-    for(i = 0; i < piece_positions.length; i++)
-    {
-        piece_pos = piece_positions[i];
-        console.log(piece_pos);
-        draw_piece(app, square_size, piece_pos[2], piece_pos[0], piece_pos[1]);
-    }
-}
-
-draw_position(app, moves[moves.length - 1].second_board_fen);
+var curr_move_index = moves.length - 1;
+var board_size = (app.screen.width / 2) - (2 * padding);
+var first_board = new ChessBoard(board_size, light_color, dark_color, piece_textures, false, pocket_size, pocket_color);
+var second_board = new ChessBoard(board_size, light_color, dark_color, piece_textures, true, pocket_size, pocket_color);
+first_board.graphics.x = padding;
+first_board.graphics.y = padding;
+second_board.graphics.x = board_size + (3 * padding);
+second_board.graphics.y = padding;
+app.stage.addChild(first_board.graphics);
+app.stage.addChild(second_board.graphics);
+redraw_boards();
--- static/js/libs/README.md
+++
@@ -1,6 +0,0 @@
-# fen-diagram
-A tiny, flexible javascript library to generate Chess diagrams from Forsyth-Edwards Notation.
-
-
-Details available on the github page!  http://houseofjeff.github.io/fen-diagram
-
